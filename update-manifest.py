27e9b4024e21a7930e6d175445408ce8e1ed1186
Will Thompson <will@willthompson.co.uk>
2018-01-29 11:38:22 +0000

    update-manifest: support writing out nightly manifest
    I don't want to manually maintain the copy in Bustle's Git repo.

--- update-manifest.py
+++ update-manifest.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python3
-import yaml
+import argparse
 import json
+import yaml
+
+
+def write_json(target, manifest):
+    with open(target, 'w', encoding='utf-8') as g:
+        json.dump(obj=manifest, fp=g, indent=4)
+        g.write('\n')
 
 
 def main():
-    '''The master copy is only YAML so we can have comments.'''
+    '''The master copy is YAML so we can have comments.'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--nightly', metavar='PATH',
+                        help='Write nightly build manifest to PATH')
+    args = parser.parse_args()
+
     with open('org.freedesktop.Bustle.yaml', 'r', encoding='utf-8') as f:
         manifest = yaml.load(f)
 
-    with open('org.freedesktop.Bustle.json', 'w', encoding='utf-8') as g:
-        json.dump(obj=manifest, fp=g, indent=4)
-        g.write('\n')
+    write_json('org.freedesktop.Bustle.json', manifest)
+
+    if args.nightly:
+        bustle_module_source = manifest['modules'][-1]["sources"][0]
+        bustle_module_source['branch'] = 'master'
+        del bustle_module_source['commit']
+
+        manifest['tags'] = ['nightly']
+        manifest['desktop-file-name-prefix'] = '(Nightly) '
+
+        write_json(args.nightly, manifest)
 
 
 if __name__ == '__main__':
